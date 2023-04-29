d412ef420adaab37bb007c81e5ecd0924a3f7b89
matef <matef@ntgclarity.com>
2023-04-29 19:42:35 +0300

    finalize stc assessment - adding dockerfile for backend and postgresql DB

--- src/main/java/com/stc_assessment/stc_assessment/StcAssessmentApplication.java
+++ src/main/java/com/stc_assessment/stc_assessment/StcAssessmentApplication.java
@@ -8,6 +8,20 @@ public class StcAssessmentApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(StcAssessmentApplication.class, args);
+		systemInit();
+	}
+
+	private static void systemInit() {
+		System.out.println("******************************************************************************************");
+		System.out.println("   _____ _______ _____                                                       _   \n" +
+				"  / ____|__   __/ ____|     /\\                                              | |  \n" +
+				" | (___    | | | |         /  \\   ___ ___  ___  ___ ___ _ __ ___   ___ _ __ | |_ \n" +
+				"  \\___ \\   | | | |        / /\\ \\ / __/ __|/ _ \\/ __/ __| '_ ` _ \\ / _ \\ '_ \\| __|\n" +
+				"  ____) |  | | | |____   / ____ \\\\__ \\__ \\  __/\\__ \\__ \\ | | | | |  __/ | | | |_ \n" +
+				" |_____/   |_|  \\_____| /_/    \\_\\___/___/\\___||___/___/_| |_| |_|\\___|_| |_|\\__|\n" +
+				"                                                                                 ");
+		System.out.println("***************************************************************** Back End Started *******");
+
 	}
 
 }
